9be22311e29a42b9083a177919105de660fa7b03
Shawn MacIntyre <shawn@selfstructured.com>
2021-06-12 21:05:03 +0700

    Crossterm working

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,71 @@
-use std::io::stdout;
+use std::io::{stdout, Write};
 use crossterm::{
-    event::read,
+    style::{
+        Color,
+        SetForegroundColor,
+        SetBackgroundColor,
+        Print,
+        ResetColor
+    },
+    event::{
+        KeyEvent,
+        Event,
+        read,
+        KeyCode
+    },
     execute,
     terminal,
-    style::{SetForegroundColor, Color, SetBackgroundColor, Print, ResetColor}
+    ExecutableCommand,
 };
-use crossterm::event::Event;
 
 fn main() -> crossterm::Result<()> {
-    terminal::enable_raw_mode()?;
+    let mut buffer = String::new();
 
     execute!(
         stdout(),
         SetForegroundColor(Color::Blue),
-        SetBackgroundColor(Color::Red),
-        Print("Styled text here."),
+        Print("> "),
         ResetColor
     )?;
 
-    match read()? {
-        Event::Key(event) => println!("{:?}", event),
-        Event::Mouse(event) => println!("{:?}", event),
-        Event::Resize(width, height) => println!("New size {}x{}", width, height),
+    terminal::enable_raw_mode()?;
+    loop {
+        match read()? {
+            Event::Key(KeyEvent { code, modifiers }) => {
+                match code {
+                    KeyCode::Backspace => {}
+                    KeyCode::Enter => {
+                        println!("{}", buffer);
+                        break;
+                    }
+                    KeyCode::Left => {}
+                    KeyCode::Right => {}
+                    KeyCode::Up => {}
+                    KeyCode::Down => {}
+                    KeyCode::Home => {}
+                    KeyCode::End => {}
+                    KeyCode::PageUp => {}
+                    KeyCode::PageDown => {}
+                    KeyCode::Tab => {}
+                    KeyCode::BackTab => {}
+                    KeyCode::Delete => {}
+                    KeyCode::Insert => {}
+                    KeyCode::F(_) => {}
+                    KeyCode::Char(c) => {
+                        let mut char_buffer = [0; 4];
+                        let bytes = c.encode_utf8(&mut char_buffer).as_bytes();
+                        stdout().write_all(bytes)?;
+                        stdout().flush()?;
+                        buffer.push(c);
+                    }
+                    KeyCode::Null => {}
+                    KeyCode::Esc => {}
+                }
+            }
+            Event::Mouse(event) => println!("{:?}", event),
+            Event::Resize(width, height) => println!("New size {}x{}", width, height),
+        }
     }
-
     terminal::disable_raw_mode()?;
 
     Ok(())
