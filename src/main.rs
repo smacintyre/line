b2587c150f39a5ae36b47b3012bc0e827d3f91de
Shawn MacIntyre <shawn@selfstructured.com>
2021-06-12 20:32:03 +0700

    Initial reading terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdout;
+use crossterm::{
+    event::read,
+    execute,
+    terminal,
+    style::{SetForegroundColor, Color, SetBackgroundColor, Print, ResetColor}
+};
+use crossterm::event::Event;
+
+fn main() -> crossterm::Result<()> {
+    terminal::enable_raw_mode()?;
+
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        Print("Styled text here."),
+        ResetColor
+    )?;
+
+    match read()? {
+        Event::Key(event) => println!("{:?}", event),
+        Event::Mouse(event) => println!("{:?}", event),
+        Event::Resize(width, height) => println!("New size {}x{}", width, height),
+    }
+
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }
